57b09bb33e46b0e6b82c53c1ef346f314b0be070
Matheus Alves <matheusalves.bmd@gmail.com>
2024-06-10 03:30:45 -0300

    Use state to add interactivity
    These are the important notes:
    
     - In React, event names are camelCased. The onClick event is one of
       many possible events you can use to respond to user interaction. For
       example, you can use onChange for input fields or onSubmit for
       forms;
    
     - React has a set of functions called hooks. Hooks allow you to add
       additional logic such as state to your components. You can think of
       state as any information in your UI that changes over time, usually
       triggered by user interaction;
    
     - Unlike props which are passed to components as the first function
       parameter, the state is initiated and store within a component. You
       can pass the state information to cildren components as props, but
       the logic for updating the state should be kept within the component
       where state was intially created.

--- index.jsx
+++ index.jsx
@@ -9,6 +9,12 @@ function Header({ title }) {
 function HomePage() {
   const names = ["Ada Lovelace", "Grace Hopper", "Margaret Hamilton"];
 
+  const [likes, setLikes] = React.useState(0);
+
+  function handleClick() {
+    setLikes(likes + 1);
+  }
+
   return (
     <div>
       {/* Nesting the Header component */}
@@ -18,6 +24,7 @@ function HomePage() {
           <li key={name}>{name}</li>
         ))}
       </ul>
+      <button onClick={handleClick}>Like ({likes})</button>
     </div>
   );
 }
